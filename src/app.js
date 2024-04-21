8834493463e8a192c66298f82aa8234a2ba770e4
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-04-21 19:41:37 +0200

    seted

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import {options, showPizza, orderShow, priceShow, btn, pizzaSlice} from "./constants.js"; // Наши константы элементов  
 import activeFunction from "./active.js" 
+// import style.css from "./"
 
 console.log(options)
 
